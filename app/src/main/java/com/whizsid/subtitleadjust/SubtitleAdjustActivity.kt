e5a8ee9e5d9695e6629fed60695bc638930d2b09
WhizSid <whizsid@aol.com>
2019-12-28 02:42:07 +0530

    Installed directory chooser

--- app/src/main/java/com/whizsid/subtitleadjust/SubtitleAdjustActivity.kt
+++ app/src/main/java/com/whizsid/subtitleadjust/SubtitleAdjustActivity.kt
@@ -1,6 +1,7 @@
 package com.whizsid.subtitleadjust
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -10,6 +11,15 @@ import com.whizsid.subtitleadjust.database.SubtitleModel
 import com.whizsid.subtitleadjust.lib.SubtitleAdjust
 import com.whizsid.subtitleadjust.lib.Time
 import kotlinx.android.synthetic.main.app_bar.*
+import android.content.Intent
+import kotlinx.android.synthetic.main.content_adjust.*
+import net.rdrei.android.dirchooser.DirectoryChooserActivity
+import net.rdrei.android.dirchooser.DirectoryChooserConfig
+
+
+
+
+
 
 class SubtitleAdjustActivity : AppCompatActivity() {
 
@@ -34,8 +44,6 @@ class SubtitleAdjustActivity : AppCompatActivity() {
         // Setting the list view adapter
         subtitleAdapter =  SubtitleAdjustListAdapter(this,listItems)
 
-
-
         val listView = findViewById<ListView>(R.id.subtitleList)
         listView?.adapter = subtitleAdapter
 
@@ -52,6 +60,32 @@ class SubtitleAdjustActivity : AppCompatActivity() {
                 subtitleAdapter.notifyDataSetChanged()
             }
         }
+
+        // Again to main activity after cancel pressed
+        val cancelButton = findViewById<Button>(R.id.adjustCancelButton)
+
+        cancelButton.setOnClickListener {
+            val newIntent = Intent(this, MainActivity::class.java)
+            this.startActivity(newIntent)
+            finish()
+        }
+
+        val confirmButton = findViewById<Button>(R.id.adjustConfirmButton)
+
+        confirmButton.setOnClickListener {
+            val chooserIntent = Intent(this, DirectoryChooserActivity::class.java)
+
+            val config = DirectoryChooserConfig.builder()
+                .newDirectoryName("DirChooserSample")
+                .allowReadOnlyDirectory(true)
+                .allowNewDirectoryNameModification(true)
+                .build()
+
+            chooserIntent.putExtra(DirectoryChooserActivity.EXTRA_CONFIG, config)
+
+            // REQUEST_DIRECTORY is a constant integer to identify the request, e.g. 0
+            startActivityForResult(chooserIntent, REQUEST_DIRECTORY)
+        }
     }
 
     override fun onBackPressed() {
@@ -63,5 +97,27 @@ class SubtitleAdjustActivity : AppCompatActivity() {
         dbHelper.empty()
 
         subtitleAdapter.notifyDataSetChanged()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_DIRECTORY) {
+            if (resultCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
+                val dir =  data!!.getStringExtra(DirectoryChooserActivity.RESULT_SELECTED_DIR)
+
+                this.directorySelected(dir)
+            } else {
+                // Nothing selected
+            }
+        }
+    }
+
+    private fun directorySelected(dir: String){
+
+    }
+
+    companion object {
+        const val REQUEST_DIRECTORY = 123
     }
 }
